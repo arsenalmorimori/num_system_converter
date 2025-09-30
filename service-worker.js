2fbfe90d49bcc4cd747c634ac61e10d27ef7a610
arsenalmorimori <arsenalbautista1@gmail.com>
2025-09-30 21:04:17 +0800

    pwa test

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ncs-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/convert.html',
+  '/index.js',
+  '/convert.js',
+  '/css.css',
+  '/assets/Cascadia.ttf',
+];
+
+// Install event: cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event: serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
